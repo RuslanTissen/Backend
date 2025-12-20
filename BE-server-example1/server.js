b67864e5f7cf8792c813ad5f57a56f9451195095
Ruslan Tissen <rthyssen73@gmail.com>
2025-12-20 12:33:10 +0100

    20.12.25

--- BE-server-example1/server.js
+++ BE-server-example1/server.js
@@ -3,6 +3,11 @@ import express from "express";
 // This creates server object
 const app = express();
 
+// First endpoint - "GET /"
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
 app.listen(3000, () => {
-  console.log("Listening to http://locolhost:3000");
+  console.log("Listening to http://localhost:3000");
 });

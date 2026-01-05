f5f0dc3a0eabfb03af9f1f9ed2a66d8bf692e68e
Ruslan Tissen <rthyssen73@gmail.com>
2026-01-05 11:22:23 +0100

    5.1.26

---
+++ BE-server-example1/fetch-curl.js
@@ -0,0 +1,44 @@
+fetch("https://www.linkedin.com/voyager/api/voyagerMessagingGraphQL/graphql?queryId=messengerMailboxCounts.fc528a5a81a76dff212a4a3d2d48e84b&variables=(mailboxUrn:urn%3Ali%3Afsd_profile%3AACoAABVQdc0BAefSPy2hNOZlHtv0PwYXEFgGRPQ)", {
+  "headers": {
+    "accept": "application/graphql",
+    "accept-language": "en-US,en;q=0.9,ru;q=0.8",
+    "csrf-token": "ajax:6808687777139167620",
+    "priority": "u=1, i",
+    "sec-ch-prefers-color-scheme": "dark",
+    "sec-ch-ua": "\"Google Chrome\";v=\"143\", \"Chromium\";v=\"143\", \"Not A(Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "x-li-lang": "en_US",
+    "x-li-page-instance": "urn:li:page:d_flagship3_profile_view_base;Hy4nIETzTkC8wTFBnpYWHg==",
+    "x-li-track": "{\"clientVersion\":\"1.13.41695\",\"mpVersion\":\"1.13.41695\",\"osName\":\"web\",\"timezoneOffset\":1,\"timezone\":\"Europe/Berlin\",\"deviceFormFactor\":\"DESKTOP\",\"mpName\":\"voyager-web\",\"displayDensity\":1,\"displayWidth\":1920,\"displayHeight\":1080}",
+    "x-restli-protocol-version": "2.0.0"
+  },
+  "referrer": "https://www.linkedin.com/in/ruslan-tissen-3799119b/",
+  "body": null,
+  "method": "GET",
+  "mode": "cors",
+  "credentials": "include"
+});
+
+curl 'https://www.linkedin.com/voyager/api/voyagerMessagingGraphQL/graphql?queryId=messengerMailboxCounts.fc528a5a81a76dff212a4a3d2d48e84b&variables=(mailboxUrn:urn%3Ali%3Afsd_profile%3AACoAABVQdc0BAefSPy2hNOZlHtv0PwYXEFgGRPQ)' \
+  -H 'accept: application/graphql' \
+  -H 'accept-language: en-US,en;q=0.9,ru;q=0.8' \
+  -b 'bcookie="v=2&377679c4-c9cc-472f-85ea-d5fd783cc691"; li_gc=MTswOzE3NTkzNDczMzg7MjswMjGARRiz+/eIYPYTGh56RNlFpg8ru6ueozOtX0W0mM9lxQ==; bscookie="v=1&20251002145502dc33510d-4049-4b3b-8be3-a3c90ebeee46AQH9Vo5doOMNP50czsobTcbiX6UPndc1"; li_alerts=e30=; dfpfpt=76e84096d9d94a3e90b33dcfbacf142c; g_state={"i_l":0}; liap=true; JSESSIONID="ajax:6808687777139167620"; li_theme=light; li_theme_set=app; timezone=Europe/Berlin; sdui_ver=sdui-flagship:0.1.20688-2+SduiFlagship0; _pxvid=c70702c8-9f9f-11f0-891a-89628f68d472; lang=v=2&lang=en-us; li_at=AQEDARVQdc0EtM1PAAABmaVrcawAAAGbrhEIiU0AYRZVWx3HhqbcLOY1Or_R9NVZ2qkrdbx54pBAjXNAn4AV_F1AuhWlQTqK02Zl38JqhG1k9H41TnNZ7nV4kWqhEFqHYMwoomsUzZH3KUBGyHifjzE2; lidc="b=VB73:s=V:r=V:a=V:p=V:g=3906:u=610:x=1:i=1767547110:t=1767549498:v=2:sig=AQGc1UaRoj88VtqX323O5C7eJXJdg0Hg"; fptctx2=taBcrIH61PuCVH7eNCyH0K%252fD9DJ44Cptuv0RyrXgXCs%252fWE65Wn057ijZR%252flm4%252bY0PE%252b4NCXloGtzfc0DlPPrdQBztADUMo9XuI37S3sJNJYBgw2j2Mhi86p1li%252bdEXxtkU1%252bEoQgX5xZJc4naWsyFeCRojxKpGOrl1BemAOmTeUy4PrU5MWUpXEWOi8ujf8w7vLij3C078lIgdqHEyZ7zSY8X8MFSbdeaMS28czVCArOp5nq33DD3f8B0hg8ilDY5b7AzSvtPo2WRZdXPnahEzVUOf2ML7H%252bTFz5%252bGRsLyN2Hgm%252f1YywDgISrAoRY5T8VNnqCWpkyq8%252bD%252bxVaLOUs%252f1JCirAgVl9qozXUAqemzs%253d; UserMatchHistory=AQKVdf5eyxlAzQAAAZuKHiCKhYmTUjV9xkGtW67bt1i9d-Bw2lWgDJVx4i4OOsy23dEM0FyrhX07r5M96mLipvWv-q3xXGs-5K7UVKKtC6RmIxCVtqU12xn14EUz1hcKjXCwpWJc0x_HniD7t7kG6o_fr-e-NgvYthCc3rrl6K_RkNWWvc1KKOM2QMiB6nR_hTzOtoos1CF9dWA3HWIDPc8LeTAgbC0KBvdcKJ2jZeu68fgnxVt4gOiTXBXUVcqboukL32MYOIBWzK78H324Iua77E4-sy8JT4o6OYi4AS-dmF64h1QcpoSID0_5GmK1d0t3hBr-8iF5G8_sjSfU; __cf_bm=ANtgrRtMZjPqn_9LmwW4Pljd.Uftj_B3A6HIoMA1KNw-1767548912-1.0.1.1-TICRidFEuQEoS.GESBE1tsD_mwBS.3tnogQ7kWtdzQ4F6p9_b6a1mpK.QO1_o.KX0ekUfHica.PK1sU.tE0dW3n9wAv67p0.U3RF6hTweqw; li_mc=MTsyMTsxNzY3NTQ4OTI0OzI7MDIxp4jfqlXByjns5tfWbgDMsx4s+0Mf+KDDCtXLpP0lv8E=' \
+  -H 'csrf-token: ajax:6808687777139167620' \
+  -H 'priority: u=1, i' \
+  -H 'referer: https://www.linkedin.com/in/ruslan-tissen-3799119b/' \
+  -H 'sec-ch-prefers-color-scheme: dark' \
+  -H 'sec-ch-ua: "Google Chrome";v="143", "Chromium";v="143", "Not A(Brand";v="24"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Linux"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'user-agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36' \
+  -H 'x-li-lang: en_US' \
+  -H 'x-li-page-instance: urn:li:page:d_flagship3_profile_view_base;Hy4nIETzTkC8wTFBnpYWHg==' \
+  -H 'x-li-track: {"clientVersion":"1.13.41695","mpVersion":"1.13.41695","osName":"web","timezoneOffset":1,"timezone":"Europe/Berlin","deviceFormFactor":"DESKTOP","mpName":"voyager-web","displayDensity":1,"displayWidth":1920,"displayHeight":1080}' \
+  -H 'x-restli-protocol-version: 2.0.0'

132233cfde540f1b0f0d104a4d960f2d427f66ca
Ruslan Tissen <rthyssen73@gmail.com>
2026-01-26 17:22:53 +0100

    26.1.26

---
+++ BE-server-exampleLowdb/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+// 1.Initializing application
+const app = express();
+// here
+
+// 2. add endpoints
+app.get("/", (req, res) => res.send("Root get!"));
+// more...
+
+// 3.listen for endpoints
+app.listen(8080, () => console.log("Up at http://localhost:8080"));

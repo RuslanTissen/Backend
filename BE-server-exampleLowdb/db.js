283dfe91b07627692a734d0c3d5104bee95ef2a2
Ruslan Tissen <rthyssen73@gmail.com>
2026-01-31 09:54:58 +0100

    31.1.26

--- BE-server-exampleLowdb/db.js
+++ BE-server-exampleLowdb/db.js
@@ -1,11 +1,17 @@
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
 
+// Lowdb setup, use a JSON file for storage
 const adapter = new JSONFile("db.json");
 const db = new Low(adapter, {});
+// db.data = db.data || {}
 
+// Read datafrom our storage file (if one exists)
+// The data that was read will go into "db.data"
 await db.read();
 
+// Set data
+db.data.posts ||= [];
 console.log("Data:", db.data);
 
 export default db;

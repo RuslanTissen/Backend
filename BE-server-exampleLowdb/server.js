9a65098722c7c3c7a09c97a8c5206337f80db227
Ruslan Tissen <rthyssen73@gmail.com>
2026-02-06 11:16:34 +0100

    6.2.26

--- BE-server-exampleLowdb/server.js
+++ BE-server-exampleLowdb/server.js
@@ -3,6 +3,7 @@ import db from "./db.js";
 
 // 1.Initializing application
 const app = express();
+app.use(express.json());
 
 // Lowdb practice
 // db.data.posts.push({ id: 1, content: "Alexander likes pancekes!" });
@@ -10,6 +11,11 @@ const app = express();
 
 // 2. add endpoints
 app.get("/", (req, res) => res.send("Root get!"));
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Root POST");
+});
 
 // 3.listen for endpoints
 app.listen(8080, () => console.log("Up at http://localhost:8080"));

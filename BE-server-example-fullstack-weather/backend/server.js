5c816aec56e21cf8c4bf463bd1c4a4f96ff4edcc
Ruslan Tissen <rthyssen73@gmail.com>
2026-02-12 18:27:54 +0100

    12.2.26

---
+++ BE-server-example-fullstack-weather/backend/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+// 1. Initialize an application
+const app = express();
+
+// 2. Add endpoints
+
+// 3. Start listening to requests
+app.listen(3000, () => console.log("listen up at http://localhost:3000/"));

a32d6f827f283f8a4041f954befa48f2c01a97c8
Ruslan Tissen <rthyssen73@gmail.com>
2026-02-14 10:46:38 +0100

    14.2.26

--- BE-server-example-fullstack-weather/backend/server.js
+++ BE-server-example-fullstack-weather/backend/server.js
@@ -1,9 +1,24 @@
 import express from "express";
+import cors from "cors";
 
 // 1. Initialize an application
 const app = express();
+app.use(cors());
+app.use(express.json()); // Parses(populates) incoming requests into req.body
 
 // 2. Add endpoints
+app.post("/weather", (req, res) => {
+  console.log("POST /weather");
+  // read city from the request
+  console.log(req.body);
+
+  // Do some stuff!!!
+
+  res.send({
+    requestedCity: req.body.city,
+    temperature: Math.round(Math.random() * 20),
+  });
+});
 
 // 3. Start listening to requests
 app.listen(3000, () => console.log("listen up at http://localhost:3000/"));

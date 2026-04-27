772974081a5d1d03e29e9b27763f91c916db2ebe
Ruslan Tissen <rthyssen73@gmail.com>
2026-04-27 16:38:04 +0200

    27.4.26

--- BE-Example-Messageboard/backend/routes/messageRouter.js
+++ BE-Example-Messageboard/backend/routes/messageRouter.js
@@ -4,16 +4,16 @@ const data = [];
 const messageRouter = express.Router();
 
 messageRouter
-  .get((req, res) => res.json(data))
-  .post((req, res) => {
+  .get("/", (req, res) => res.json(data))
+  .post("/", (req, res) => {
     // Assumption: request contains a body with a valid message object
     req.body.id = Date.now();
     data.push(req.body);
     res.sendStatus(201);
   })
 
-  .put(":/id", (req, res) => res.sendStatus(501))
-  .patch(":/id", (req, res) => res.sendStatus(501))
-  .delete(":/id", (req, res) => res.sendStatus(501));
+  .put("/:id", (req, res) => res.sendStatus(501))
+  .patch("/:id", (req, res) => res.sendStatus(501))
+  .delete("/:id", (req, res) => res.sendStatus(501));
 
 export default messageRouter;

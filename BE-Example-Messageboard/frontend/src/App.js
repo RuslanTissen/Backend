3fbe47d9474293866e64003c5e00f30caa614f10
Ruslan Tissen <rthyssen73@gmail.com>
2026-04-28 20:50:35 +0200

    28.4.26

--- BE-Example-Messageboard/frontend/src/App.js
+++ BE-Example-Messageboard/frontend/src/App.js
@@ -1,7 +1,27 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Form } from "react-bootstrap";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <h1>Message board</h1>
+      <Form>
+        <Form.Group className="mb-3">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="text" placeholder="Name" />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={3} placeholder="Message" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default App;
